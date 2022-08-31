3c145bdfa127dd7647345df4b167bf31390398d1
arindampradhan <arindampradhan10@gmail.com>
2022-08-31 14:55:52 +0530

    setup of lint-staged :check

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,4 +7,5 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '**/*.(md|json)': filenames => `prettier --write ${filenames.join(' ')}`
 }
