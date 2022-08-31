1fb255a6cc3c9b3c8c94141c8beee71e1a34f0ea
arindampradhan <arindampradhan10@gmail.com>
2022-09-01 00:41:36 +0530

    feat: add redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { wrapper } from '../store';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(MyApp);
+const MyTranslatedApp = appWithTranslation(MyApp);
+const MyReduxApp = wrapper.withRedux(MyTranslatedApp);
+
+export default MyReduxApp;
