4fe94a76bdd1b6ff82f8acdb5cdcacd5df6fef4a
arindam-zeta <arindamp@zeta.tech>
2022-09-13 02:27:03 +0530

    fix: build issues and update it

--- src/additional.d.ts
+++ src/additional.d.ts
@@ -1,1 +1,7 @@
-//
+// more declarations
+// FIXME: type defination not working
+declare module 'components/**/*.svg' {
+  import React from 'react';
+  const Component: React.FunctionComponent<React.SVGProps<SVGAElement>>;
+  export default Component;
+}
